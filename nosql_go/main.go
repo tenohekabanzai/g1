ea7d88df613efd5b76b1dac0bcb31c18b9f1ab3b
tenohekabanzai <10ohekabanzai@gmail.com>
2025-02-24 17:10:28 +0530

    first

---
+++ nosql_go/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type User struct {
+	ID    primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name  string             `bson:"name" json:"name"`
+	Email string             `bson:"email" json:"email"`
+}
+
+var client *mongo.Client
+var userCollection *mongo.Collection
+
+func init() {
+
+	err_env := godotenv.Load()
+	if err_env != nil {
+		log.Fatal("Error loading .env file")
+	}
+	mongoURI := os.Getenv("MONGO_URI")
+
+	var err error
+	client, err = mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal("MongoDB Connection Error:", err)
+	}
+
+	userCollection = client.Database("users").Collection("users")
+	fmt.Println("Connected to MongoDB!")
+}
+
+// Create User
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	// fmt.Println(user)
+	user.ID = primitive.NewObjectID()
+
+	_, err := userCollection.InsertOne(context.TODO(), user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+// Get All Users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	cursor, err := userCollection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	for cursor.Next(context.TODO()) {
+		var user User
+		cursor.Decode(&user)
+		users = append(users, user)
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+// Get User by ID
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var user User
+	err = userCollection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&user)
+	if err != nil {
+		http.Error(w, "User Not Found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+// Update User
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	update := bson.M{"$set": user}
+	_, err = userCollection.UpdateOne(context.TODO(), bson.M{"_id": id}, update)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+// Delete User
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	_, err = userCollection.DeleteOne(context.TODO(), bson.M{"_id": id})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "User deleted"})
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Routes
+	r.HandleFunc("/users", GetUsers).Methods("GET")
+	r.HandleFunc("/user/{id}", GetUser).Methods("GET")
+	r.HandleFunc("/user", CreateUser).Methods("POST")
+	r.HandleFunc("/user/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/user/{id}", DeleteUser).Methods("DELETE")
+
+	// Start Server
+	fmt.Println("Server is running on port 5004")
+	log.Fatal(http.ListenAndServe(":5004", r))
+}
