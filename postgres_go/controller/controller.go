2f0aab36727e8618614aef59abf330944c86066b
tenohekabanzai <10ohekabanzai@gmail.com>
2025-02-26 00:38:53 +0530

    changes

--- postgres_go/controller/controller.go
+++ postgres_go/controller/controller.go
@@ -1,4 +1,4 @@
-package middleware
+package controller
 
 import (
 	"database/sql"
--- postgres_go/router/router.go
+++ postgres_go/router/router.go
@@ -1,17 +1,17 @@
 package router
 
 import (
-	"postgres_go/middleware"
+	"postgres_go/controller"
 
 	"github.com/gorilla/mux"
 )
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET")
-	r.HandleFunc("/api/stock", middleware.GetAllStocks).Methods("GET")
-	r.HandleFunc("/api/newstock", middleware.AddStock).Methods("POST")
-	r.HandleFunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT")
-	r.HandleFunc("/api/stock/{id}", middleware.DeleteStock).Methods("DELETE")
+	r.HandleFunc("/api/stock/{id}", controller.GetStock).Methods("GET")
+	r.HandleFunc("/api/stock", controller.GetAllStocks).Methods("GET")
+	r.HandleFunc("/api/newstock", controller.AddStock).Methods("POST")
+	r.HandleFunc("/api/stock/{id}", controller.UpdateStock).Methods("PUT")
+	r.HandleFunc("/api/stock/{id}", controller.DeleteStock).Methods("DELETE")
 	return r
 }
