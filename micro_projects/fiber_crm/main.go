3570228c9b968a6ba42484f5b27e0991f8197251
tenohekabanzai <10ohekabanzai@gmail.com>
2025-02-27 18:16:49 +0530

    fiber2

--- micro_projects/fiber_crm/main.go
+++ micro_projects/fiber_crm/main.go
@@ -16,8 +16,11 @@ type Lead struct {
 	Phone   int    `json:"phone"`
 }
 
+var db *gorm.DB
+
 func initDB() {
-	var db, err = gorm.Open(sqlite.Open("leads.db"), &gorm.Config{})
+	var err error
+	db, err = gorm.Open(sqlite.Open("leads.db"), &gorm.Config{})
 	if err != nil {
 		panic("Err connecting to DB")
 	}
@@ -26,12 +29,66 @@ func initDB() {
 	if err != nil {
 		panic("Failed to migrate to DB")
 	}
+
+}
+
+func Getleads(c *fiber.Ctx) error {
+	var leads []Lead
+	err := db.Find(&leads)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.JSON(leads)
+	return nil
+}
+func Getlead(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var lead Lead
+	err := db.Find(&lead, id)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.JSON(lead)
+	return nil
+}
+func Addlead(c *fiber.Ctx) error {
+	var lead Lead
+	err := c.BodyParser(&lead)
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.Create(&lead)
+	c.JSON(lead)
+	return nil
+}
+func Deletelead(c *fiber.Ctx) error {
+
+	id := c.Params("id")
+	var lead Lead
+	err := db.Find(&lead, id)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	// if lead.Name == "" {
+	// 	c.Status(500).Send([]byte("Cannot find lead with given id"))
+	// 	return nil
+	// }
+	db.Delete(&lead)
+	c.Send([]byte("deleted lead successfully"))
+	return nil
 }
 
 func main() {
+
 	initDB()
 	fmt.Println("Fiber CRUD App with sqlite3")
-
 	app := fiber.New()
+	// routes
+	app.Get("/api/leads", Getleads)
+	app.Get("/api/lead/:id", Getlead)
+	app.Post("/api/lead", Addlead)
+	app.Delete("/api/lead/:id", Deletelead)
 	app.Listen(":5004")
+
 }
